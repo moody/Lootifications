da8566f4a2233ad76acfb0afc45dd7a76f8f83d4
moody <moody@users.noreply.github.com>
2023-12-31 19:21:34 -0500

    update anchor frame text

--- src/locale.lua
+++ src/locale.lua
@@ -11,6 +11,7 @@ setmetatable(L, {
 -- English
 -- ============================================================================
 
+L["ANCHOR"] = "Anchor"
 L["COMMAND_DESCRIPTION_ANCHOR"] = "Toggle the notifications anchor."
 L["COMMAND_DESCRIPTION_HELP"] = "Display a list of commands."
 L["COMMAND_DESCRIPTION_MAX"] = "Set the maximum number of displayed notifications (%s-%s)."
