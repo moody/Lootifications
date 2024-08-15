5ad0fbf2bb76212950a523371aed2b25cc3d1ced
moody <moody@users.noreply.github.com>
2024-08-15 14:20:05 -0400

    update widgets

--- src/ui/widgets/_init.lua
+++ src/ui/widgets/_init.lua
@@ -1,15 +1,8 @@
-local _, Addon = ...
+local Addon = select(2, ...) ---@type Addon
+
+--- @class Widgets
 local Widgets = Addon:GetModule("Widgets")
 
---[[
-  ```lua
-  -- Example usage:
-  Mixin(frame, BackdropTemplateMixin)
-  frame:SetBackdrop(Widgets.BORDER_BACKDROP)
-  frame:SetBackdropColor(0, 0, 0, 0.75)
-  frame:SetBackdropBorderColor(0, 0, 0, 1)
-  ```
-]]
 Widgets.BORDER_BACKDROP = {
   bgFile = "Interface\\Buttons\\WHITE8x8",
   edgeFile = "Interface\\Buttons\\WHITE8x8",
@@ -22,6 +15,9 @@ do -- Widget:Padding()
   local PADDING = 8
   local cache = {}
 
+  --- Returns a base padding of `8` multiplied by the given number.
+  --- @param multiplier? number
+  --- @return number padding
   function Widgets:Padding(multiplier)
     if type(multiplier) ~= "number" then return PADDING end
     local value = cache[tostring(multiplier)]
