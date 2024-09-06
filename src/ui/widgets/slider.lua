2e9b7e81cdef820ad94d4999dd1c1f0547040d7c
moody <moody@users.noreply.github.com>
2024-09-06 17:26:50 -0400

    update slider colors

--- src/ui/widgets/slider.lua
+++ src/ui/widgets/slider.lua
@@ -25,7 +25,6 @@ local Widgets = Addon:GetModule("Widgets")
 function Widgets:Slider(options)
   -- Defaults.
   options.frameType = "Slider"
-  options.onUpdateTooltip = nil
   options.width = Addon:IfNil(options.width, 12)
   options.height = Addon:IfNil(options.height, 12)
   options.orientation = Addon:IfNil(options.orientation, "VERTICAL")
@@ -36,7 +35,7 @@ function Widgets:Slider(options)
   --- @class SliderWidget : FrameWidget, Slider
   local frame = self:Frame(options)
   frame:SetBackdropColor(Colors.DarkGrey:GetRGBA(0.5))
-  frame:SetBackdropBorderColor(Colors.DarkGrey:GetRGBA(0.5))
+  frame:SetBackdropBorderColor(Colors.Purple:GetRGBA(0.5))
 
   frame:SetObeyStepOnDrag(true)
   frame:SetOrientation(options.orientation)
@@ -46,7 +45,7 @@ function Widgets:Slider(options)
 
   -- Thumb texture.
   frame.texture = frame:CreateTexture("$parent_Texture", "ARTWORK")
-  frame.texture:SetColorTexture(Colors.White:GetRGBA(0.25))
+  frame.texture:SetColorTexture(Colors.Purple:GetRGBA(0.75))
   frame:SetThumbTexture(frame.texture)
 
   -- Set texture size.
@@ -59,12 +58,12 @@ function Widgets:Slider(options)
   end
 
   frame:SetScript("OnMouseDown", function()
-    frame.texture:SetColorTexture(Colors.White:GetRGBA(0.5))
+    frame.texture:SetColorTexture(Colors.Purple:GetRGBA())
   end)
 
-  frame:SetScript("OnMouseUp", function(self)
-    frame.texture:SetColorTexture(Colors.White:GetRGBA(0.25))
-    if options.set then options.set(self:GetValue()) end
+  frame:SetScript("OnMouseUp", function()
+    frame.texture:SetColorTexture(Colors.Purple:GetRGBA(0.75))
+    if options.set then options.set(frame:GetValue()) end
   end)
 
   return frame
